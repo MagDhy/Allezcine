6ce6a1db7674769484ae71e6d3618b1a42f7787c
Magali Dhyne <magalidhyne@live.be>
2020-07-22 16:32:49 +0200

    finish aside, add nav vertic, add features movies, add shop movies

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,13 @@ $('.dropdown-toggle').dropdown();
 $('.carousel').carousel({
     interval: 2000
 });
+
+//call collapse element
+$('.collapse').collapse();
+
+//créer les 5 cartes du premier deck avec une boucle for
+//appel d'API voir vidéos postman puis authentification movies database
+//tableaux dans lequels on stocke les informations (+ async awaits)
+
+
+//cacher le bouton remonter jusqu'à % page
